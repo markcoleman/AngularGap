78d0b8adaf4f8116cd41c3a4653adf3c245c8492
Mark Coleman <markcoleman@gmail.com>
2013-05-22 20:38:16 -0400

    AngularGap, Step 1

---
+++ www/js/app.js
@@ -0,0 +1,4 @@
+var angulargap = angular.module("angulargap", []);
+angulargap.controller("HomeController", function($scope){
+                      $scope.message = "AngularJS!";
+                      });
