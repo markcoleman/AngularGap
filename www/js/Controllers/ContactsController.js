03e7845fff87cfb3bf63f4b9a4846ac1b158f981
Mark Coleman <markcoleman@gmail.com>
2013-06-30 13:05:22 -0400

    finished up contact service

--- www/js/Controllers/ContactsController.js
+++ www/js/Controllers/ContactsController.js
@@ -1,13 +1,13 @@
 angulargap.controller("ContactsController", function ($scope, ContactService) {
 
+    $scope.state = "find";
 
     $scope.contacts = [];
 
     $scope.contact = {};
 
-    $scope.findContact = function () {
-        console.log($scope.filter);
-        ContactService.find($scope.filter).then(function (contacts) {
+    $scope.findContact = function (contactSearch) {
+        ContactService.find(contactSearch).then(function (contacts) {
             $scope.contacts = contacts;
         }, function (error) {
             console.log(error);
@@ -15,14 +15,33 @@ angulargap.controller("ContactsController", function ($scope, ContactService) {
     };
 
     $scope.edit = function (contact) {
+        $scope.state="create";
         $scope.contact = contact;
     };
 
-    $scope.create = function () {
-        $scope.contact = ContactService.create();
-        $scope.action = 'create'
+    $scope.cancel = function(){
+        $scope.state="find";
+        $scope.contact = null;
     };
 
+    $scope.showFindContact = function(){
+        $scope.state="find";
+        $scope.contact = null;
+        $scope.contacts = [];
+    };
+
+    $scope.showCreateContact = function () {
+        $scope.state = 'create';
+        $scope.contact = ContactService.create();
+    };
+    $scope.back = function(){
+        $scope.state="find";
+    };
+    $scope.view = function(contact){
+        $scope.state="view";
+        $scope.contact = contact;
+
+    };
     $scope.save = function () {
         $scope.contact.save();
     };
--- www/js/Services/ContactService.js
+++ www/js/Services/ContactService.js
@@ -3,16 +3,15 @@ angulargap.factory("ContactService", function ($rootScope, $q) {
 
     return {
         create: function () {
-            return navigator.contacts.create();
+            return navigator.contacts.create()
         },
         find: function (filter) {
-
             var deferred = $q.defer();
 
             var options = new ContactFindOptions();
             options.filter = filter;
             options.multiple = true;
-            var fields = ["displayName", "name"];
+            var fields = ["displayName", "name", "addresses", "emails"];
             navigator.contacts.find(fields, function (contacts) {
                 $rootScope.$apply(function () {
                     deferred.resolve(contacts);
